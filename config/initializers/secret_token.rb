5e184fe9fbe2a178ca63d24ebceea583e39d9b80
Nicholas Manos <manos.nick@gmail.com>
2014-09-23 19:37:42 -0700

    Adding basic authenticate features created from scratch using railscast #250

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '63ed16184a812d8dc9ba3120b05e5f3da219cd5082e90a320922436bdf26f3d78de975e710c47f45062dbf1f24332681dc56dfb460e73e4a7728806375e20d7e'
